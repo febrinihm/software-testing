12937e6500d74abaee1ea27a04ae8362cd9f434a
febrinihm <febrimalau2@gmail.com>
2022-11-26 13:20:04 +0700

    add caesarshift test bug

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,20 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CaesarShiftCipherTest {
 
+    @Test
+    public void testmessage(){
+        CaesarShiftCipher shift = new CaesarShiftCipher();
+        String result = shift.CaesarShiftCipher("abc",3);
+        Assertions.assertEquals("def", result);
+    }
+    @Test
+    public void testmessage2(){
+        CaesarShiftCipher shift = new CaesarShiftCipher();
+        String result = shift.CaesarShiftCipher("xyz", 3);
+        Assertions.assertEquals("abc", result);
+    }
 }
