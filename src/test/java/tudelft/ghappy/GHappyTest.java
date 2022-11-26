b2ebac1b7f6613d58ad67cca34a91afcfc2698c1
febrinihm <febrimalau2@gmail.com>
2022-11-26 11:31:01 +0700

    add happy test

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,25 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+
+    @Test
+    public void test1(){
+        GHappy happy = new GHappy();
+        Assertions.assertTrue(happy.gHappy("xxggxx"));
+    }
+
+    @Test
+    public void test2(){
+        GHappy happy = new GHappy();
+        Assertions.assertFalse(happy.gHappy("xxgxx"));
+    }
+
+    @Test
+    public void test3(){
+        GHappy happy = new GHappy();
+        Assertions.assertFalse(happy.gHappy("xxggyygxx"));
+    }
 }
